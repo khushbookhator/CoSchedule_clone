606078dea8f1798dd2154497bfd27f3cf3d841cf
Satwik Nayak <satwikn07@gmail.com>
2021-03-11 20:12:54 +0530

    renamed

--- navbar.js
+++ navbar.js
@@ -1,3 +1,14 @@
-window.onscroll=function(e){
-    document.getElementById('navbar')
-}
+// window.onscroll=function(e){
+//     document.getElementById('navbar')
+// }
+var lastScrollTop = 0;
+var navbar = document.getElementsByClassName('navbar');
+window.addEventListener('scroll',function(){
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if(scrollTop>lastScrollTop){
+        navbar.style.top="-80px"
+    }else{
+        navbar.style.top = "0";
+    }
+    lastScrollTop = scrollTop;
+})
