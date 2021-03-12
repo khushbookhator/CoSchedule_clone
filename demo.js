e7bf1ffb16361166c9c974877b3c6d262b07d214
sunil kumar <ssunil.uk@gmail.com>
2021-03-12 13:48:40 +0530

    add files

--- demo.js
+++ demo.js
@@ -1,3 +1,6 @@
+var date;
+var slot;
+
 window.onload = function() {
     let time = document.getElementById('time')
     let current =new Date()
@@ -53,27 +56,84 @@ showMarch = () => {
         }
     }
 }
-timeIn = () => {
+
+timeIn = (event) => {
+    let arr = ['12:00am', '12:15am', '12:30am', '12:45am', '1:00am', '1:15am', '1:30am', '1:45am', '2:00am', '2:15am', '2:30am', '2:45am','3:00am', '3:15am', '3:30am', '3:45am','4:00am', '4:15am', '4:30am', '4:45am','5:00am', '5:15am', '5:30am', '5:45am','6:00am', '6:15am', '6:30am', '6:45am', '7:00am', '7:15am']
+
+     date=event.target.innerText
     
     let bottom =  document.getElementById('bottom')
     let left = document.getElementById("left")
     let right = document.getElementById('right')
-    var rightnew = document.createElement('div')
-    bottom.style.width = "85%"
+    let rightnew = document.getElementById('result')
+    document.getElementById('result').innerHTML = ""
+
+    bottom.style.width = "75%"
     left.style.width = "25%"
     right.style.width = "40%"
     rightnew.style.width = "30%"
     rightnew.style.paddingTop = "12%"
-    bottom.append(rightnew)
+    rightnew.style.overflowY = "scroll"
+    
     rightnew.innerText = ""
     for(var i=0; i<30; i++) {
-        var btns = document.createElement('button')
-        console.log("a")
-        btns.innerText = i
-        
+        var btns = document.createElement('div')
+        btns.style.display = "flex"
+        var btnsT = document.createElement('button')
+        var btnsC = document.createElement('button')
+        btnsT.setAttribute('name', `${arr[i]}`)
+        btnsT.setAttribute('class', `subs`)
+        btnsT.innerText = `${arr[i]}`
+       
         rightnew.appendChild(btns)
-        // secs.style.width = "50px"
-        btns.style.border = "red"
+        btns.appendChild(btnsT)
+        btns.appendChild(btnsC)
+        btnsC.style.display = "none"
+        btnsC.style.width = "20%"
+
+        btnsT.addEventListener('mouseover', function(event){
+            event.target.style.border = "2px solid rgb(17, 162, 247)"
+        })
+        btnsT.addEventListener('mouseout', function(event) {
+            event.target.style.border = "2px solid #c4e9ff"
+        })
+        btnsT.addEventListener('click', askConfirm)
+        btnsC.addEventListener('click', confirmed)
     }
-    
+    bottom.append(rightnew)   
 }
+function askConfirm(event) {
+    event.target.style.background = "grey"
+    event.target.style.width = "40%"
+    event.target.style.color = "white"
+    event.target.style.border = "1px solid grey"
+     slot=event.target.name
+    event.target.nextSibling.style.display = "block"
+    event.target.nextSibling.style.width = "40%"
+    event.target.nextSibling.style.height = "50px"
+    event.target.nextSibling.style.background = "rgb(17, 162, 247)"
+    event.target.nextSibling.style.marginLeft = "5px"
+    event.target.nextSibling.style.marginTop = "5px"
+    event.target.nextSibling.style.borderRadius = "5px"
+    event.target.nextSibling.style.border = "none"
+    event.target.nextSibling.style.color = "white"
+    event.target.nextSibling.style.fontSize = "14px"
+    event.target.nextSibling.style.fontWeight = "700"
+    event.target.nextSibling.innerText = "Confirm"
+} 
+function confirmed(event) {
+    event.preventDefault()
+    let url = ('confirming.html?')
+    var monthv = document.getElementById('mar').textContent
+    let params = new URLSearchParams()
+    params.append('month', `${monthv}`)
+    params.append('date', `${date}`)
+    params.append('slot', `${slot}`)
+    window.location.assign(url + params.toString())
+
+}
+
+
+
+
+
