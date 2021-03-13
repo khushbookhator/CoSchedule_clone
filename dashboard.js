c62a77a574755846b8f247389e18f6e409e5cd7a
khushbookhator <khushbookhator11@gmail.com>
2021-03-13 10:02:21 +0530

    new added

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,4 @@
+var localnm;
 window.onload = () => {
     let data = localStorage.getItem('localuser')
     data = JSON.parse(data)
@@ -10,11 +11,16 @@ window.onload = () => {
     n.style.fontWeight = '700'
     // localem = user[0].email;
     // console.log(localem)
+    let funs = document.getElementById('funs')
+    funs.addEventListener('click', showdatas)
 }
-
 
 showMeNotification =() => {
     event.preventDefault()
+    let hide = document.getElementById('assigned')
+    hide.style.display = "none"
+    let task = document.getElementById('task')
+    task.style.display = "none"
     let not = document.getElementById('notify')
     let rec = document.getElementById('recent')
     let fav = document.getElementById('fav')
@@ -22,17 +28,62 @@ showMeNotification =() => {
     fav.style.display = "none"
     rec.style.display = "none"
 }
+showdatas = () => {
+    document.getElementById('showData').innerHTML = ""
+    document.getElementById('pone').innerHTML = ""
+    document.getElementById('ptwo').innerHTML = ""
+    document.getElementById('pthree').innerHTML = ""
+    document.getElementById('choose').innerText = ""
+    document.getElementById('notify').innerHTML = ""
+    document.getElementById('recent').innerHTML = ""
+    document.getElementById('fav').innerHTML = ""
+    let datas = document.getElementById('showData')
+    let n = document.getElementById('username').textContent
+    let loginman = document.getElementById('loginman')
+    loginman.innerText = n
+    loginman.style.color = "#2A4564"
+
+    let hide = document.getElementById('assigned')
+    let task = document.getElementById('task')
+    task.style.display = "block"
+    hide.style.display = "block"
+    let dd = document.getElementById('dropD')
+    dd.style.display = "block"
+
+    let padd = document.getElementById('datas')
+    padd.style.padding = "0px"
+    let choose = document.getElementById('choose')
+    let b = document.createElement('p')
+    b.innerText = "Edit Task"
+    choose.append(b)
+    b.style.marginLeft = "10px"
+    let p = document.createElement('input')
+    p.style.width= "80%";
+    p.style.border = "none"
+    p.style.height = "50px"
+    p.placeholder = "Task name..."
+    datas.append(p)
+}
 showMeRecents =() => {
     event.preventDefault()
     let rec = document.getElementById('recent')
     let not = document.getElementById('notify')
     let fav = document.getElementById('fav')
+    let hide = document.getElementById('assigned')
+    let task = document.getElementById('task')
+    task.style.display = "none"
+    hide.style.display = "none"
+
     rec.style.display = "block"
     not.style.display = "none"
     fav.style.display = "none"
 }
 showMeFav =() => {
     event.preventDefault()
+    let hide = document.getElementById('assigned')
+    hide.style.display = "none"
+    let task = document.getElementById('task')
+    task.style.display = "none"
     let fav = document.getElementById('fav')
     let rec = document.getElementById('recent')
     let not = document.getElementById('notify')
@@ -48,6 +99,4 @@ stopUser = () => {
     let pops = document.getElementById('pops')
     pops.style.display = "none"
 }
-document.getElementById("btn")
-console.log(document.getElementById("btn"))
 {/* <input type="text" id="right" name="right" oninput="left.value = right.value; return true;" /> */}
